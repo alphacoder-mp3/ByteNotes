1334cbd100005c75951dca50efe7e41edf8d5501
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-08-16 21:40:38 +0530

    Update color palette component

--- components/color-palette.tsx
+++ components/color-palette.tsx
@@ -1,5 +1,6 @@
 import { Color } from '@/hooks/useColorPalette';
 import { Dispatch, RefObject, SetStateAction } from 'react';
+import { getLightModeColor, getDarkModeColor } from '@/common/common';
 
 export const ColorPalette = ({
   colorPaletteRef,
@@ -12,12 +13,14 @@ export const ColorPalette = ({
 }) => {
   return (
     <div
-      className="fixed inset-0 max-w-full h-12 px-2 py-4 md:py-0 bg-zinc-900 flex items-center justify-center flex-wrap gap-2 rounded-xl z-50"
+      className="fixed inset-0 max-w-full h-12 px-2 py-4 md:py-0 bg-zinc-900 flex items-center justify-center flex-wrap gap-2 dark:rounded-xl z-50"
       ref={colorPaletteRef}
     >
       {bgColors.map(color => (
         <div
-          className={`${color} h-6 w-6 rounded-full hover:border border-white cursor-pointer`}
+          className={`h-6 w-6 rounded-full hover:border border-white cursor-pointer ${getLightModeColor(
+            color
+          )} ${getDarkModeColor(color)}`}
           key={color}
           onClick={() => setBgColor(color)}
         />
