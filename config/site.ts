592fa1ef7180dad87fb8d82c78ebaa1556899672
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-08-16 21:09:45 +0530

    Fix route of github repo

--- config/site.ts
+++ config/site.ts
@@ -20,7 +20,7 @@ export const siteConfig = {
     'Uncover the depth of my skills and experience as a programmer through my portfolio, where each project reflects my passion for problem-solving and dedication to excellence in software engineering. ',
   links: {
     twitter: 'https://twitter.com/folklore69_',
-    github: 'https://github.com/alphacoder-mp3',
+    github: 'https://github.com/alphacoder-mp3/ByteNotes',
     linkedin: 'https://www.linkedin.com/in/akshayshindeconnects/',
     instagram: 'instagram.com',
   },
