ee6ff86e7fb81983504844c219cb0edd694cf3c3
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-08-17 08:45:20 +0530

    Update creation of note

--- components/create-notes.tsx
+++ components/create-notes.tsx
@@ -63,7 +63,7 @@ export default function CreateNotes({ userId }: { userId: string }) {
     <form ref={formRef} onSubmit={handleSubmit} className="space-y-4">
       <div className="w-full flex justify-center">
         <div
-          className={`w-screen md:w-3/5 lg:w-2/5 border border-slate-500 rounded-lg ${
+          className={`w-screen md:w-3/5 lg:w-2/5 shadow dark:border border-slate-500 rounded-lg ${
             isExpanded
               ? `h-auto p-4 ${bgColor ? `${bgColor} border-none` : ''}`
               : 'h-11'
@@ -88,7 +88,7 @@ export default function CreateNotes({ userId }: { userId: string }) {
                 id="title"
                 placeholder="Title"
                 required
-                className={`w-full p-2 border-b border-slate-700 mb-2 outline-none ${
+                className={`w-full p-2 dark:border-b border-slate-700 mb-2 outline-none ${
                   bgColor ? bgColor : ''
                 }`}
               />
@@ -99,7 +99,7 @@ export default function CreateNotes({ userId }: { userId: string }) {
                 rows={1}
                 required
                 autoFocus
-                className={`w-full p-2 border-b border-slate-700 mb-2 outline-none resize-none ${
+                className={`w-full p-2 dark:border-b border-slate-700 mb-2 outline-none resize-none ${
                   bgColor ? bgColor : ''
                 }`}
                 onKeyDown={handleKeyDown}
