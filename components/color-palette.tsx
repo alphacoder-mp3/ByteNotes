5e2f674beba9314dc2b053ae6b343184679067ef
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-08-17 06:13:01 +0530

    Add color palette ui update

--- components/color-palette.tsx
+++ components/color-palette.tsx
@@ -18,14 +18,14 @@ export const ColorPalette = ({
   return (
     <div
       className={cn(
-        'fixed inset-0 max-w-full min-h-fit md:h-12 px-2 py-4 md:py-0 bg-zinc-900 flex items-center justify-center flex-wrap gap-2 dark:rounded-xl z-50',
+        'fixed inset-0 max-w-full min-h-fit md:h-12 px-2 py-4 md:py-0 dark:bg-zinc-900 bg-zinc-100 flex items-center justify-center flex-wrap gap-2 dark:rounded-xl z-50',
         className
       )}
       ref={colorPaletteRef}
     >
       {bgColors.map(color => (
         <div
-          className={`h-6 w-6 rounded-full hover:border border-white cursor-pointer ${getLightModeColor(
+          className={`h-6 w-6 rounded-full hover:border dark:border-white border-black cursor-pointer ${getLightModeColor(
             color
           )} ${getDarkModeColor(color)}`}
           key={color}
